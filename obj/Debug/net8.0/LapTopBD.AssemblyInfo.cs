e219c396e33ce623891fc97ea05e24c983cdf75d
phi hung <hungnp@inmobi.vn>
2026-04-12 11:32:48 +0700

    Update database

--- obj/Debug/net8.0/LapTopBD.AssemblyInfo.cs
+++ obj/Debug/net8.0/LapTopBD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LapTopBD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a5eca58e4db5ff5405bfbc5692d5d70d7c60e11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24f3805bd9df9bbec437846d8a9541a20835e43b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LapTopBD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LapTopBD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
