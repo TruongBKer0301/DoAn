083bd060419023dd03bc21a56758c05dde7f0ea9
phi hung <hungnp@inmobi.vn>
2026-04-13 17:34:37 +0700

    add average rating

--- obj/Debug/net8.0/LapTopBD.AssemblyInfo.cs
+++ obj/Debug/net8.0/LapTopBD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LapTopBD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a784411dba6b92734824d7fbc8e62ea534be7786")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c420bca311a29455fecea0ac868ebc4faf1bcfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("LapTopBD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LapTopBD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
