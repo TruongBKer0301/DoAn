3afd7909d29041be1ddfc77a56f147eb150cf802
TruongBKer0301 <truong.nguyen0301@hcmut.edu.vn>
2026-05-14 21:57:06 +0700

    done

--- obj/Debug/net8.0/LapTopBD.AssemblyInfo.cs
+++ obj/Debug/net8.0/LapTopBD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LapTopBD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+656ffa66e0e396ecf8fa33ca4d2b0ae8b21335c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3487d88b4f7a20325537cd6b1d192361d10729c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LapTopBD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LapTopBD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
